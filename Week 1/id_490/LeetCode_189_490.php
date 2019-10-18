1137b0a4ecc7aa7f9adce3f633cbbd868e03006a
Yu Gang <yugang@xin.com>
2019-10-18 08:45:37 +0800

    提交作业

--- Week 1/id_490/LeetCode_189_490.php
+++ Week 1/id_490/LeetCode_189_490.php
@@ -1,5 +1,11 @@
 <?php
 
+/**
+ * 第一种解法：暴力求解，双重循环，时间复杂度O(n^2),空间复杂度O(1)
+ * @param Integer[] $nums
+ * @param Integer $k
+ * @return NULL
+ */
 function rotate(&$nums, $k) {
     for ($i = 1; $i <= $k; $i++) {
         $length = count($nums);
@@ -10,3 +16,31 @@ function rotate(&$nums, $k) {
         $nums[0] = $tmp;
     }
 }
+
+/**
+ * 解法2，使用三次反转，时间复杂度O(n),空间复杂度O(1)
+ * @param Integer[] $nums
+ * @param Integer $k
+ * @return NULL
+ */
+function rotate2(&$nums, $k) {
+    $len = count($nums);
+    $k = $k % $len;
+    reverse($nums, 0, $len - 1);
+    reverse($nums, 0, $k - 1);
+    reverse($nums, $k, $len - 1);
+}
+
+function reverse(&$nums, $start, $end) {
+    while ($start < $end) {
+        $temp = $nums[$start];
+        $nums[$start] = $nums[$end];
+        $nums[$end] = $temp;
+        $start++;
+        $end--;
+    }
+}
+
+$arr = [1, 2, 3, 4, 5, 6, 7];
+rotate2($arr, 3);
+var_dump($arr);
