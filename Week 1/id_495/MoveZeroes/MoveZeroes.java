9e8c24e51877349415c68dee7b6b4071048085c3
wangshaodi <fengzhong_yue@163.com>
2019-10-19 18:46:42 +0800

    算法作业

---
+++ Week 1/id_495/MoveZeroes/MoveZeroes.java
@@ -0,0 +1,26 @@
+package MoveZeroes;
+
+public class MoveZeroes {
+
+  public static void main(String[] args) {
+    int[] nums = {0,1,0,3,12};
+    moveZeroes(nums);
+    for (int a: nums) {
+      System.out.print(a);
+      System.out.print(",");
+    }
+  }
+
+  public static void moveZeroes(int[] nums) {
+
+    int j = 0;
+    for(int i = 0; i < nums.length; i++) {
+        if(nums[i] != 0) {
+            int temp = nums[j];
+            nums[j] = nums[i];
+            nums[i] = temp;
+            j++;
+        }
+    }
+  }
+}
