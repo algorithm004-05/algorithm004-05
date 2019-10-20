f208716a34161a8ae717338c9adaaf0459e8d37f
CindyYu759 <835895687@qq.com>
2019-10-20 20:44:12 +0800

    week1接雨水

--- Week 1/id_460/Question42TrappingRainWater/TrappingRainWater.java
+++ Week 1/id_460/Question42TrappingRainWater/TrappingRainWater.java
@@ -1,5 +1,7 @@
 package Question42TrappingRainWater;
 
+import java.util.Stack;
+
 import static java.lang.Math.max;
 import static java.lang.Math.min;
 
@@ -11,6 +13,7 @@ public class TrappingRainWater {
         int height[] = {0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};
         System.out.println(trap(height));
         System.out.println(trap1(height));
+        System.out.println(trap2(height));
     }
 
     private static int trap(int[] height) {
@@ -59,7 +62,27 @@ public class TrappingRainWater {
         }
         return max;
     }
-
+    private static int trap2(int[] height) {
+        int sum = 0;
+        Stack<Integer> stack = new Stack<>();
+        int current = 0;
+        while (current < height.length) {
+            //如果栈不空并且当前指向的高度大于栈顶高度就一直循环
+            while (!stack.empty() && height[current] > height[stack.peek()]) {
+                int h = height[stack.peek()]; //取出要出栈的元素
+                stack.pop(); //出栈
+                if (stack.empty()) { // 栈空就出去
+                    break;
+                }
+                int distance = current - stack.peek() - 1; //两堵墙之前的距离。
+                int min = Math.min(height[stack.peek()], height[current]);
+                sum = sum + distance * (min - h);
+            }
+            stack.push(current); //当前指向的墙入栈
+            current++; //指针后移
+        }
+        return sum;
+    }
 }
 
 
