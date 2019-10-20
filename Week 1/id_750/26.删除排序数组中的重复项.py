17515e92f742a75e9f5315fd792bcd5f815e113e
zxj <zhangxiaojian@xuanye100.com>
2019-10-20 19:16:28 +0800

    750-week-01

---
+++ Week 1/id_750/26.删除排序数组中的重复项.py
@@ -0,0 +1,19 @@
+from typing import List
+
+
+# 26.删除排序数组中的重复项
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        if not nums: return 0
+        k = 1
+        for i in range(1, len(nums)):
+            if nums[i] != nums[i - 1]:
+                nums[k] = nums[i]
+                k += 1
+        return k
+
+
+if __name__ == '__main__':
+    s = Solution()
+    a = [1, 1, 2]
+    print(s.removeDuplicates(a))
