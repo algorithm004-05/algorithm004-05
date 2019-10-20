67ca830f803787585fc288e1ce8d5d82ecc78677
CindyYu759 <835895687@qq.com>
2019-10-20 20:42:09 +0800

    week1接雨水

--- Week 1/id_460/Question42TrappingRainWater/TrappingRainWater.java
+++ Week 1/id_460/Question42TrappingRainWater/TrappingRainWater.java
@@ -10,13 +10,14 @@ public class TrappingRainWater {
     public static void main(String[] args) {
         int height[] = {0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};
         System.out.println(trap(height));
+        System.out.println(trap1(height));
     }
 
     private static int trap(int[] height) {
         int result = 0;
         int size = height.length;
 
-        for (int i = 0; i < size ; i++) {
+        for (int i = 0; i < size; i++) {
             int max_left = 0, max_right = 0;
             for (int j = i; j >= 0; j--) {
                 max_left = max(max_left, height[j]);
@@ -28,6 +29,37 @@ public class TrappingRainWater {
         }
         return result;
     }
+
+    private static int trap1(int[] height) {
+        int sum = 0;
+        int max = getMax(height);//找到最大的高度，以便遍历。
+        for (int i = 1; i <= max; i++) {
+            boolean isStart = false; //标记是否开始更新 temp
+            int temp_sum = 0;
+            for (int j = 0; j < height.length; j++) {
+                if (isStart && height[j] < i) {
+                    temp_sum++;
+                }
+                if (height[j] >= i) {
+                    sum = sum + temp_sum;
+                    temp_sum = 0;
+                    isStart = true;
+                }
+            }
+        }
+        return sum;
+    }
+
+    private static int getMax(int[] height) {
+        int max = 0;
+        for (int i = 0; i < height.length; i++) {
+            if (height[i] > max) {
+                max = height[i];
+            }
+        }
+        return max;
+    }
+
 }
 
 
