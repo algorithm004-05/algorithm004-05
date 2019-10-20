88a5d474bd058f28f630eb16811a2853eb243997
lzxzy <chuangmenlzx@gmail.com>
2019-10-20 21:36:34 +0800

    add homework stack

--- Week 1/id_195/LeetCode_189_195.py
+++ Week 1/id_195/LeetCode_189_195.py
@@ -1,6 +1,6 @@
 
 class Solution:
-    def rotate_1(self, nums: List[int], k: int) -> None:
+    def rotate_1(self, nums, k) -> None:
         """
         Do not return anything, modify nums in-place instead.
         """
@@ -10,7 +10,7 @@ class Solution:
                 nums[j] = nums[j-1]
             nums[0] = tmp
 
-    def rotate_2(self, nums: List[int], k: int) -> None:
+    def rotate_2(self, nums, k) -> None:
         length = len(nums)
         k %= length
         count = start = 0 #start 是最开始的点
@@ -25,22 +25,24 @@ class Solution:
                     break
             start += 1
 
-    def rotate_3(self, nums: List[int], k: int) -> None:
+    def rotate_3(self, nums, k) -> None:
         k %= len(nums)
         self.reverse(nums, 0, len(nums) - 1)
         self.reverse(nums, 0, k-1)
         self.reverse(nums, k, len(nums) - 1)
-    def reverse(self, nums: List[int], start: int, end: int) -> None:
+    def reverse(self, nums, start, end) -> None:
         while start < end:
             nums[start], nums[end] = nums[end], nums[start]
             start += 1
             end -= 1
 
-    def rotate(self, nums: List[int], k: int) -> None:
+    def rotate(self, nums, k) -> None:
         return self.rotate_3(nums, k)
 
 if __name__ == '__main__':
     testCase = [1, 3, 4, 5, 6]
     k = 6
+    print("Origin: ", testCase, " k:", k)
     solution = Solution()
-    print(solution.rotate(testCase, k))
+    solution.rotate(testCase, k)
+    print("Rotated: ", testCase)
