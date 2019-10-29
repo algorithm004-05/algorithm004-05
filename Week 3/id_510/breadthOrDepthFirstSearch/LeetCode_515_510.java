ee5c9b0216315ecbf520b48604850d72a52597e7
changyg <changyg@shpso.com>
2019-10-29 18:15:49 +0800

    提交类型

---
+++ Week 3/id_510/breadthOrDepthFirstSearch/LeetCode_515_510.java
@@ -0,0 +1,40 @@
+package algorithm00405test.week3.lesson.todo.breadthOrDepthFirstSearch;
+
+
+import java.util.List;
+
+/**
+ *您需要在二叉树的每一行中找到最大的值。
+ *
+ * 示例：
+ *
+ * 输入:
+ *
+ *           1
+ *          / \
+ *         3   2
+ *        / \   \
+ *       5   3   9
+ *
+ * 输出: [1, 3, 9]
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/find-largest-value-in-each-tree-row
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class LeetCode_515_510 {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public List<Integer> largestValues(TreeNode root) {
+        return null;
+    }
+}
