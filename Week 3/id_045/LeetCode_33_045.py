d7db0c18d3671e2018b820f822fe6fed53fff904
lichangke <986740304@qq.com>
2019-11-03 23:38:13 +0800

    add week 03

---
+++ Week 3/id_045/LeetCode_33_045.py
@@ -0,0 +1,28 @@
+# @author:leacoder
+# @des:  二分查找 搜索旋转排序数组
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+
+        if not nums:
+            return -1
+
+        low , high = 0, len(nums) - 1
+
+        while low <= high:
+            mid = (low + high) // 2
+            if target == nums[mid]:
+                return mid
+
+            if nums[low] <= nums[mid]:
+                if nums[low] <= target <= nums[mid]:
+                    high = mid - 1
+                else:
+                    low = mid + 1
+            else:
+                if nums[mid] <= target <= nums[high]:
+                    low = mid + 1
+                else:
+                    high = mid - 1
+        return -1
+
