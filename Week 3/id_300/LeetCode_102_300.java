ac1a22843753b67c9526c54779c330f741a1a8ea
zhangxu-git <33169312+zhangxu-git@users.noreply.github.com>
2019-11-03 21:10:24 +0800

    Create LeetCode_102_300.java

---
+++ Week 3/id_300/LeetCode_102_300.java
@@ -0,0 +1,33 @@
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+
+        List<List<Integer>> levels = new ArrayList<List<Integer>>();
+        if (root == null) return levels;
+
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.add(root);
+        int level = 0;
+        while ( !queue.isEmpty() ) {
+            // start the current level
+            levels.add(new ArrayList<Integer>());
+
+            // number of elements in the current level
+            int level_length = queue.size();
+            for(int i = 0; i < level_length; ++i) {
+                TreeNode node = queue.remove();
+
+                // fulfill the current level
+                levels.get(level).add(node.val);
+
+                // add child nodes of the current level
+                // in the queue for the next level
+                if (node.left != null) queue.add(node.left);
+                if (node.right != null) queue.add(node.right);
+            }
+            // go to next level
+            level++;
+        }
+        return levels;
+
+    }
+}
