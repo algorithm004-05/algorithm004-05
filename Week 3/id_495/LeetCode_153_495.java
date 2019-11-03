77f58bbb8b1ca35f44918c19d95653e96144798a
wangshaodi <fengzhong_yue@163.com>
2019-11-03 12:58:26 +0800

    chore!: update LeetCode_153_495.java BREAKING CHANGE: 原有的算法最坏时间复杂度为O(n),现在该用二分法查找，O(log(n))

--- Week 3/id_495/LeetCode_153_495.java
+++ Week 3/id_495/LeetCode_153_495.java
@@ -1,17 +1,16 @@
 class LeetCode_153_495 {
     public static void main(String[] args) {
-        System.out.println(findMin(new int[] {4,5,6,7,0,1,2,3}));
+        System.out.println(findMin(new int[] {4,5,6,7,1,2,3}));
     }
 
     public static int findMin(int[] nums) {
-        int len=nums.length;
-        int p1=0,p2=len-1;
-        if(nums[0]<=nums[len-1])return nums[0];
-        while(nums[p1]>nums[p2]){
-            if(p2==0)return nums[0];
-            p2--;
-
-        }
-        return nums[p2+1];
+      int left = 0;
+      int right = nums.length - 1;
+      while (left < right) {
+          int mid = left + (right - left) / 2;
+          if (nums[mid] > nums[right]) left = mid + 1;
+          else right = mid;
+      }
+      return nums[left];
     }
 }
