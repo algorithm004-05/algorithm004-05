1a18741cd0ceba6dd91c05c567ac32f6754a78b6
lzxzy <chuangmenlzx@gmail.com>
2019-11-10 21:37:49 +0800

    update week 3 homework

--- Week 2/id_195/LeetCode_50_195.py
+++ Week 2/id_195/LeetCode_50_195.py
@@ -28,3 +28,18 @@ class Solution:
             x = 1/x
             n = -n
         return self.fastPow(x, n)
+
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        judge = True
+        if n < 0:
+            n = -n
+            judge = False
+        final = 1
+        while n > 0:
+            if n % 2==0:
+                x *= x
+                n //= 2
+            final *= x
+            n -= 1
+        return final if judge else 1/final
