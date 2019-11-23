055da2db2357faa6f1b35f78a5da8a285893819a
dorianHe <dorianh1994@gmail.com>
2019-11-23 10:28:26 +0100

    bfs & dfs based solution added

--- Week 6/id_755/LeetCode_547_755.py
+++ Week 6/id_755/LeetCode_547_755.py
@@ -1,3 +1,4 @@
+# using disjoint set
 class disjoint_set:
     def __init__(self, num):
         self.array = [i for i in range(num)]
@@ -31,3 +32,51 @@ class Solution:
                 if M[i][j] == 1:
                     dset.union(i, j)
         return len(set([dset.find(i) for i in dset.array]))
+    
+# using dfs
+class Solution:
+    def findCircleNum(self, M: List[List[int]]) -> int:
+        n = len(M)
+        if n == 0:
+            return 0
+        m = len(M[0])
+        visited = set()
+        count = 0
+        
+        def dfs(index, value):
+            if index in visited or value == 0:
+                return
+            visited.add(index)
+            neighbor_lst = M[index]
+            for neighbor, access in enumerate(neighbor_lst):
+                if neighbor not in visited and access == 1:
+                    dfs(neighbor, access)
+
+        for i in range(n):
+            if i not in visited:
+                dfs(i, 1)
+                count += 1
+        return count
+
+# using bfs
+class Solution:
+    def findCircleNum(self, M: List[List[int]]) -> int:
+        n = len(M)
+        if n == 0:
+            return 0
+        m = len(M[0])
+        queue = collections.deque()
+        visited = set()
+        count = 0
+        for i in range(n):
+            if i not in visited:
+                queue.append(i)
+                while queue:
+                    node = queue.popleft()
+                    visited.add(node)
+                    node_lst = M[node]
+                    for neighbor, access in enumerate(node_lst):
+                        if neighbor not in visited and access == 1:
+                            queue.append(neighbor)
+                count += 1
+        return count
