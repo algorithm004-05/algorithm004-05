7490f8bbf33228c94f5066db06dd2aadc0f2e212
dingxi <XiDing@sf-express.com>
2019-11-24 23:38:34 +0800

    第六周作业

--- Week 6/id_240/LeetCode_208_240.js
+++ Week 6/id_240/LeetCode_208_240.js
@@ -1,29 +1,17 @@
-### 字典树
-
-1. 节点本身不存在完整单词
-2. 从根节点到某一结点，路径上经过的字符连接起来，为该节点对应的字符串
-3. 每个节点的所有子节点路径代表的字符都不相同
-4. 字典树典型应用情景就是搜索提示词
-5. javascript实现字典树
-
-~~~javascript
 /**
  * Initialize your data structure here.
  */
 var Trie = function() {
-
     this.nextLetter = {};
     this.isWord = false;
-
-}; 
+};
 
 /**
- * Inserts a word into the trie.
+ * Inserts a word into the trie. 
  * @param {string} word
  * @return {void}
  */
 Trie.prototype.insert = function(word) {
-
     let cur = this;
     let arr = word.split('');
     for(let i = 0,len = arr.length;i<len;i++){
@@ -35,16 +23,14 @@ Trie.prototype.insert = function(word) {
         }
     }
     cur.isWord = true;
-
-}; 
+};
 
 /**
- * Returns if the word is in the trie.
+ * Returns if the word is in the trie. 
  * @param {string} word
  * @return {boolean}
  */
 Trie.prototype.search = function(word) {
-
     let cur = this;
     let arr = word.split('');
     for(let i = 0,len = arr.length;i<len;i++){
@@ -55,16 +41,14 @@ Trie.prototype.search = function(word) {
         }
     }
     return cur.isWord;
-
-}; 
+};
 
 /**
- * Returns if there is any word in the trie that starts with the given prefix.
+ * Returns if there is any word in the trie that starts with the given prefix. 
  * @param {string} prefix
  * @return {boolean}
  */
 Trie.prototype.startsWith = function(prefix) {
-
     let cur = this;
     let arr = prefix.split('');
     for(let i = 0,len = arr.length;i<len;i++){
@@ -75,9 +59,4 @@ Trie.prototype.startsWith = function(prefix) {
         }
     }
     return true;
-
-}; 
-~~~
-
-  
-
+};
