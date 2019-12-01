be220249a1b084602490f815974da628ef1f1894
fforange <55771390+fforange@users.noreply.github.com>
2019-12-01 16:33:37 +0800

    260-week07

---
+++ Week 7/id_260/260-week 07/leetcode_1122_260.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def relativeSortArray(self, arr1, arr2):
+        """
+        :type arr1: List[int]
+        :type arr2: List[int]
+        :rtype: List[int]
+        """
+        arr = [0 for _ in range(1001)]
+        res = []
+        for i in arr1:
+            arr[i] +=1
+        for i in arr2:
+            while arr[i] > 0:
+                res.append(i)
+                arr[i] -=1
+        for i in range(len(arr)):
+            while arr[i] > 0:
+                res.append(i)
+                arr[i] -=1
+        return res 
