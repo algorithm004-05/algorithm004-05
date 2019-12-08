cac0cd3bb1211d50936234c08f6ece38677e55cf
wangshaodi <fengzhong_yue@163.com>
2019-12-08 23:37:55 +0800

    feat: week 08 Homework

---
+++ Week 08/id_495/LeetCode_438_495.java
@@ -0,0 +1,36 @@
+class LeetCode_438_495 {
+    public List<Integer> findAnagrams(String s, String p) {
+        if(s == null || s.length() == 0) return new ArrayList<>();
+        List<Integer> res = new ArrayList<>();
+        int[] needs = new int[26]; //由于都是小写字母，因此直接用26个长度的数组代替原来的HashMap
+        int[] window = new int[26];
+        int left = 0, right = 0, total = p.length(); //用total检测窗口中是否已经涵盖了p中的字符
+        for(char ch : p.toCharArray()){
+            needs[ch - 'a'] ++;
+        }
+        while(right < s.length()){
+            char chr = s.charAt(right);
+            if(needs[chr - 'a'] > 0){
+                window[chr - 'a'] ++;
+                if(window[chr - 'a'] <= needs[chr - 'a']){
+                    total --;
+                }
+            }
+            while(total == 0){
+                if(right-left+1 == p.length()){
+                    res.add(left);
+                }
+                char chl = s.charAt(left);
+                if(needs[chl - 'a'] > 0){
+                    window[chl - 'a'] --;
+                    if(window[chl - 'a'] < needs[chl - 'a']){
+                        total ++;
+                    }
+                }
+                left ++;
+            }
+            right ++;
+        }
+        return res;
+    }
+}
