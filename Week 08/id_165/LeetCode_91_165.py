8fdf5026d9703e3fd81c9e72ca7549caa0f631dd
reonard <reonard@126.com>
2019-12-08 18:00:06 +0800

    week 8

---
+++ Week 08/id_165/LeetCode_91_165.py
@@ -0,0 +1,40 @@
+# 一条包含字母 A-Z 的消息通过以下方式进行了编码：
+#
+# 'A' -> 1
+# 'B' -> 2
+# ...
+# 'Z' -> 26
+#
+#
+# 给定一个只包含数字的非空字符串，请计算解码方法的总数。
+#
+# 示例 1:
+#
+# 输入: "12"
+# 输出: 2
+# 解释: 它可以解码为 "AB"（1 2）或者 "L"（12）。
+#
+#
+# 示例 2:
+#
+# 输入: "226"
+# 输出: 3
+# 解释: 它可以解码为 "BZ" (2 26), "VF" (22 6), 或者 "BBF" (2 2 6) 。
+#
+# Related Topics 字符串 动态规划
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution(object):
+    def numDecodings(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        pp, p = 1, int(s[0] != '0')
+        for i in range(1, len(s)):
+            pp, p = p, pp * (9 < int(s[i-1:i+1]) <= 26) + p * (int(s[i]) > 0)
+        return p
+
+
+# leetcode submit region end(Prohibit modification and deletion)
