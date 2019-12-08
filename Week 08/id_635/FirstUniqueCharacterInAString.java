6a4c1ffc5df42fd22c8c64daf394013ad58ec775
SimonChu80 <12984384@qq.com>
2019-12-08 18:56:31 +0800

    635 - week 08

---
+++ Week 08/id_635/FirstUniqueCharacterInAString.java
@@ -0,0 +1,25 @@
+package com.simon.leetcode.w8.lcn387;
+
+import java.util.HashMap;
+
+public class FirstUniqueCharacterInAString {
+
+
+    public int firstUniqChar(String s) {
+        HashMap<Character, Integer> count = new HashMap<Character, Integer>();
+        int n = s.length();
+
+        for (int i = 0; i < n; i++) {
+            char c = s.charAt(i);
+            count.put(c, count.getOrDefault(c, 0) + 1);
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (count.get(s.charAt(i)) == 1)
+                return i;
+        }
+        return -1;
+    }
+
+
+}
