8a22624488d1922f728e4ba0b09756377cbae7f0
laojinxing <lavenderistie@gmail.com>
2019-12-09 15:12:39 +0800

    065-Week 08

---
+++ Week 08/id_065/LeetCode_8_065.java
@@ -0,0 +1,40 @@
+/*
+ * @lc app=leetcode.cn id=8 lang=java
+ *
+ * [8] 字符串转换整数 (atoi)
+ */
+
+// @lc code=start
+class Solution {
+    public int myAtoi(String str) {
+        str = str.trim();
+        if (str == null || str.length() == 0)
+            return 0;
+
+        char firstChar = str.charAt(0);
+        int sign = 1;
+        int start = 0;
+        long res = 0;
+        if (firstChar == '+') {
+            sign = 1;
+            start++;
+        } else if (firstChar == '-') {
+            sign = -1;
+            start++;
+        }
+
+        for (int i = start; i < str.length(); i++) {
+            if (!Character.isDigit(str.charAt(i))) {
+                return (int) res * sign;
+            }
+            res = res * 10 + str.charAt(i) - '0';
+            if (sign == 1 && res > Integer.MAX_VALUE)
+                return Integer.MAX_VALUE;
+            if (sign == -1 && res > Integer.MAX_VALUE)
+                return Integer.MIN_VALUE;
+        }
+        return (int) res * sign;
+    }
+}
+// @lc code=end
+
