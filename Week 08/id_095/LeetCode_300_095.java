dda3915137b7e0d1f432ac733c1a61160cc74ba5
jill2834 <nxt2834@hotmail.com>
2019-12-09 12:25:09 +0800

    095-Week 08

---
+++ Week 08/id_095/LeetCode_300_095.java
@@ -0,0 +1,58 @@
+/*
+ * @lc app=leetcode id=300 lang=java
+ *
+ * [300] Longest Increasing Subsequence
+ *
+ * https://leetcode.com/problems/longest-increasing-subsequence/description/
+ *
+ * algorithms
+ * Medium (41.71%)
+ * Likes:    3328
+ * Dislikes: 75
+ * Total Accepted:    288.4K
+ * Total Submissions: 691.3K
+ * Testcase Example:  '[10,9,2,5,3,7,101,18]'
+ *
+ * Given an unsorted array of integers, find the length of longest increasing
+ * subsequence.
+ * 
+ * Example:
+ * 
+ * 
+ * Input: [10,9,2,5,3,7,101,18]
+ * Output: 4 
+ * Explanation: The longest increasing subsequence is [2,3,7,101], therefore
+ * the length is 4. 
+ * 
+ * Note: 
+ * 
+ * 
+ * There may be more than one LIS combination, it is only necessary for you to
+ * return the length.
+ * Your algorithm should run in O(n^2) complexity.
+ * 
+ * 
+ * Follow up: Could you improve it to O(n log n) time complexity?
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        int[] dp = new int[nums.length];
+        int len = 0;
+        for (int num : nums) {
+            int i = Arrays.binarySearch(dp, 0, len, num);
+            if (i < 0) {
+                i = -(i + 1);
+            }
+            dp[i] = num;
+            if (i == len) {
+                len++;
+            }
+        }
+        return len;
+    }
+}
+// @lc code=end
+
---
+++ Week 08/id_095/LeetCode_387_095.java
@@ -0,0 +1,48 @@
+/*
+ * @lc app=leetcode id=387 lang=java
+ *
+ * [387] First Unique Character in a String
+ *
+ * https://leetcode.com/problems/first-unique-character-in-a-string/description/
+ *
+ * algorithms
+ * Easy (51.02%)
+ * Likes:    1312
+ * Dislikes: 92
+ * Total Accepted:    354.8K
+ * Total Submissions: 694.6K
+ * Testcase Example:  '"leetcode"'
+ *
+ * 
+ * Given a string, find the first non-repeating character in it and return it's
+ * index. If it doesn't exist, return -1.
+ * 
+ * Examples:
+ * 
+ * s = "leetcode"
+ * return 0.
+ * 
+ * s = "loveleetcode",
+ * return 2.
+ * 
+ * 
+ * 
+ * 
+ * Note: You may assume the string contain only lowercase letters.
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public int firstUniqChar(String s) {
+        int freq [] = new int[26];
+        for(int i = 0; i < s.length(); i ++)
+            freq [s.charAt(i) - 'a'] ++;
+        for(int i = 0; i < s.length(); i ++)
+            if(freq [s.charAt(i) - 'a'] == 1)
+                return i;
+        return -1;
+    }
+}
+// @lc code=end
+
