cc08b2598b2053d38ecc768ed001bbe99e403aa5
yanliang <345545854@qq.com>
2019-12-09 18:00:04 +0800

    no message

---
+++ Week 08/id_580/LeetCode_10_580.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isMatch(String s, String p) {
+        int m = s.length(), n = p.length();
+        boolean[][] f = new boolean[m + 1][n + 1];
+        
+        f[0][0] = true;
+        for(int i = 2; i <= n; i++){
+            f[0][i] = f[0][i - 2] && p.charAt(i - 1) == '*';
+        }
+        
+        for(int i = 1; i <= m; i++){
+            for(int j = 1; j <= n; j++){
+                if(s.charAt(i - 1) == p.charAt(j - 1) || p.charAt(j - 1) == '.'){
+                    f[i][j] = f[i - 1][j - 1];
+                }
+                if(p.charAt(j - 1) == '*'){
+                    f[i][j] = f[i][j - 2] || 
+                    f[i - 1][j] && (s.charAt(i - 1) == p.charAt(j - 2) || p.charAt(j - 2) == '.');
+                }
+            }
+        }
+        
+        return f[m][n];
+    }
+}
